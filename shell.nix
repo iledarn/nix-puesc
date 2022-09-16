7a26cd4be43a3aa235355fcc00b82cacc0881dfe
iledarn <iledarn@gmail.com>
2022-09-16 20:09:49 +0800

    nix - pip

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ with pkgs;
 let
   my-python-packages = python-packages: with python-packages; [
     ptpython
+    pip
     # other python packages you want
   ];
   python-with-my-packages = python3.withPackages my-python-packages;
