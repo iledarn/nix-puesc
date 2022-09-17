7d058e96f414c06cef9d6145b20398ecd2c2fb36
iledarn <iledarn@gmail.com>
2022-09-17 10:56:23 +0800

    add zeep logs

--- puesc_adapter.py
+++ puesc_adapter.py
@@ -2,14 +2,38 @@ import os
 import logging
 
 logging.basicConfig(level=logging.INFO)
-# logging.getLogger("suds.client").setLevel(logging.DEBUG)
-# logging.getLogger("suds.transport").setLevel(logging.DEBUG)
-# logging.getLogger("suds.xsd.schema").setLevel(logging.DEBUG)
+logging.getLogger("suds.client").setLevel(logging.DEBUG)
+logging.getLogger("suds.transport").setLevel(logging.DEBUG)
+logging.getLogger("suds.xsd.schema").setLevel(logging.DEBUG)
 # logging.getLogger("suds.wsdl").setLevel(logging.DEBUG)
+
+import logging.config
+
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "formatters": {"verbose": {"format": "%(name)s: %(message)s"}},
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "verbose",
+            },
+        },
+        "loggers": {
+            "zeep.transports": {
+                "level": "DEBUG",
+                "propagate": True,
+                "handlers": ["console"],
+            },
+        },
+    }
+)
 
 
 from suds.client import Client as SudsClient
 from zeep import Client as ZeepClient
+from zeep.plugins import HistoryPlugin
 
 username = os.environ["TEST_PUESC_LOGIN"]
 password = os.environ["TEST_PUESC_PASSWORD"]
@@ -18,4 +42,5 @@ test_url = "https://te-ws.puesc.gov.pl/seap_wsChannel/DocumentHandlingPort?wsdl"
 prod_url = "https://te-ws.puesc.gov.pl/seap_wsChannel/DocumentHandlingPort?wsdl"
 
 suds_client = SudsClient(test_url, cache=None)
-zeep_client = ZeepClient(test_url)
+history = HistoryPlugin()
+zeep_client = ZeepClient(test_url, plugins=[history])
